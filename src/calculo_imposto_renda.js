b77f47e15b69c6bdfd80599304193cb10f13c495
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-05-14 20:45:01 -0300

    Exercicio 3

--- src/calculo_imposto_renda.js
+++ src/calculo_imposto_renda.js
@@ -1,8 +1,8 @@
 function calculoImpostoRenda(salarioBruto) {
     let imposto = 0;
-    if (salarioBruto > 2112.00 and salarioBruto <= 2826.65) {
+    if (salarioBruto > 2112.00 && salarioBruto <= 2826.65) {
         imposto = salarioBruto * 0.075;
-    }else if (salarioBruto <= 3751.05) {
+    } else if (salarioBruto <= 3751.05) {
         imposto = salarioBruto * 0.15;
     } else if (salarioBruto <= 4664.68) {
         imposto = salarioBruto * 0.225;
