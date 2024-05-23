c9f4bf4263a099b815371c4384bd45b44b006730
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-05-23 11:31:35 -0300

    Exercicio 7

---
+++ ExerciciosSemana2/exerc07.js
@@ -0,0 +1,19 @@
+// Descrição: Crie uma função compor que recebe duas funções f e g e retorna uma nova função que é a
+//  composição de f e g (ou seja, f(g(x))).
+
+function compor(f, g) {
+    return function (x) {
+        return f(g(x));
+    }
+}
+
+function somar1(x) {
+    return x + 1;
+}
+
+function multiplicar2(x) {
+    return x * 2;
+}
+
+let funcaoComposta = compor(somar1, multiplicar2);
+console.log(funcaoComposta(5)); // 11
